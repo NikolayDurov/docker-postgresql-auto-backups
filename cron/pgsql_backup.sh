de1685698d5ba6255ecdc937bc3250c0d06f0cb8
Nick Durov <mail@durov.net>
2024-02-13 17:11:30 +0300

    fix backup scripts

--- cron/pgsql_backup.sh
+++ cron/pgsql_backup.sh
@@ -12,9 +12,9 @@ TIMESTAMP=$(date +'%Y%m%d%H%M%S')
 # Backup a Postgresql database into a daily file.
 #
 # Loop through all databases (excluding system databases)
-psql -h localhost -U "$LOGIN" -d postgres -Atc "SELECT datname FROM pg_database WHERE datistemplate = false" | while read -r DBNAME; do
+psql -h localhost -U "$POSTGRES_USER" -d postgres -Atc "SELECT datname FROM pg_database WHERE datistemplate = false" | while read -r DBNAME; do
     BACKUP_FILE="$BACKUP_DIR${DBNAME}_${TIMESTAMP}.tar"
-    pg_dump --no-owner --no-acl --dbname="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432/${DBNAME}" -Fc -o > "$BACKUP_FILE"
+    pg_dump --no-owner --no-acl --dbname="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432/${DBNAME}" -Fc > "$BACKUP_FILE"
     if [ $? -eq 0 ]; then
         echo "Backup completed for database $DBNAME. File saved as: $BACKUP_FILE"
     else
